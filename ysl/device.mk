2c514e9d301ecb57504dd17dd135c538e9d57cdc
Krittin Chuaiphikroh <peak@codenameysl.org>
2019-01-25 10:34:03 +0000

    Fixing Fingerprint
    Signed-off-by: Krittin Chuaiphikroh <peak@codenameysl.org>

--- ysl/device.mk
+++ ysl/device.mk
@@ -39,4 +39,4 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service.xiaomi_ysl
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_ysl
