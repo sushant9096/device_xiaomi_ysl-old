12a9bd8372bf82ecad3df244f0d7111a886c1cfd
Peak and Sushant <peak@codenameysl.org>
2019-01-26 11:06:03 +0000

    Added
    Signed-off-by: Peak and Sushant <peak@codenameysl.org>

--- ysl/BoardConfig.mk
+++ ysl/BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 # Inherit from common msm8953-common
 -include device/xiaomi/msm8953-common/BoardConfigCommon.mk
 
+# Device Path
 DEVICE_PATH := device/xiaomi/ysl
 
 # Kernel
