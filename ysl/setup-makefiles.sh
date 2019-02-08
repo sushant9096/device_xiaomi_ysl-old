d3359db2035994b1074d13fe5983c8fc282d1061
sushant zope <sushantzope9096@gmail.com>
2019-02-08 05:39:10 +0530

    Revert "covert common tree to full device tree"
    This reverts commit 112be762c2b5ee08189794de95fd54b5b919ca94.

--- ysl/setup-makefiles.sh
+++ ysl/setup-makefiles.sh
@@ -19,50 +19,10 @@
 set -e
 
 # Required!
-export DEVICE=rosy
+export DEVICE=ysl
 export DEVICE_COMMON=msm8953-common
 export VENDOR=xiaomi
 
 export DEVICE_BRINGUP_YEAR=2018
 
-INITIAL_COPYRIGHT_YEAR=2018
-
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-LINEAGE_ROOT="$MY_DIR"/../../..
-
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
-
-# Copyright headers and guards
-write_headers "rosy"
-
-# The standard common blobs
-write_makefiles "$MY_DIR"/proprietary-files-qc.txt true
-
-# We are done!
-write_footers
-
-if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt true
-
-    # We are done!
-    write_footers
-fi
+./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
